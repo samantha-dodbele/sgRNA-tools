af2d33f40d18dedef0da770b7b65f8150862c93f
samantha-dodbele <59745443+samantha-dodbele@users.noreply.github.com>
2020-06-25 14:48:01 -0400

    Adding Fasta conversion logic

---
+++ convert-to-fasta.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+# Read in xlsx file named 'guides' on desktop
+df = pd.read_excel(r'C:\Users\Samantha\Desktop\guides.xlsx')
+
+length = len(df["Guides"])
+fasta = []
+for x in range(0, length):
+    fasta.append("> seq"+str(x+1))
+    fasta.append(str(df["Guides"][x]))
+
+with open('./output/fasta.txt','w') as f:
+    f.write('\n'.join(fasta))
+
