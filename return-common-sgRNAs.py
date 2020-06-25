9507b0b7fcac2b5ac6ba8fd5d1d8191dae52a95c
samantha-dodbele <59745443+samantha-dodbele@users.noreply.github.com>
2020-06-25 17:09:04 -0400

    script to find common sgrnas

---
+++ return-common-sgRNAs.py
@@ -0,0 +1,22 @@
+# return common sgRNAs
+
+import pandas as pd
+
+# Read in xlsx file named 'guides' on desktop
+df = pd.read_excel(r'.\input\compareguides.xlsx')
+
+list1 = df['Guides'].tolist()
+list2 = df['Guides2'].tolist()
+list3 = df['Guides3'].tolist()
+
+s_list1 = set(list1)
+s_list2 = set(list2)
+s_list3 = set(list3)
+
+intersection_1 = s_list1.intersection(s_list2)
+intersection_2 = intersection_1.intersection(s_list3)
+intersection_as_list = list(intersection_2)
+
+
+with open('./output/common_sgrnas.txt', 'w') as f:
+    f.write('\n'.join(intersection_as_list))
