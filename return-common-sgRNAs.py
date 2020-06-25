1478aa7005ec931cfebf62d3cb8cd4af0b669961
samantha-dodbele <59745443+samantha-dodbele@users.noreply.github.com>
2020-06-25 18:03:03 -0400

    commented to compare intersection of 2 lists

--- return-common-sgRNAs.py
+++ return-common-sgRNAs.py
@@ -7,15 +7,16 @@ df = pd.read_excel(r'.\input\compareguides.xlsx')
 
 list1 = df['Guides'].tolist()
 list2 = df['Guides2'].tolist()
-list3 = df['Guides3'].tolist()
+#list3 = df['Guides3'].tolist()
 
 s_list1 = set(list1)
 s_list2 = set(list2)
-s_list3 = set(list3)
+#s_list3 = set(list3)
 
 intersection_1 = s_list1.intersection(s_list2)
-intersection_2 = intersection_1.intersection(s_list3)
-intersection_as_list = list(intersection_2)
+#intersection_2 = intersection_1.intersection(s_list3)
+#intersection_as_list = list(intersection_2)
+intersection_as_list = list(intersection_1)
 
 
 with open('./output/common_sgrnas.txt', 'w') as f:
