df6cc85331c58ad26ac242987323c2f24af8f901
samantha-dodbele <59745443+samantha-dodbele@users.noreply.github.com>
2020-12-10 18:41:36 -0500

    fixed legend

--- box-whisker.py
+++ box-whisker.py
@@ -10,43 +10,12 @@ import seaborn as sns
 df = pd.read_csv(r'.\input\box.txt', sep='\t')
 print(df.head(0))
 
-# I1 = df[df['Sample']=='I1']
-# I1.shape
-
-# I2 = df[df['Sample']=='I2']
-# I2.shape
-
-
 bp= sns.boxplot(x = 'Sample', y = "%GFP cells", data = df, width=0.8, palette="colorblind", hue= "Day")
 bp= sns.stripplot(x='Sample', y= '%GFP cells' , data=df, jitter=True, dodge=True, marker='o', alpha=0.75, color='black', hue="Day")
-# bp.get_legend().remove()
-# # get legend information from the plot object
-labels = bp.get_legend_handles_labels()
-handles= bp.get_legend_handles_labels()
-print(labels)
-print(handles)
-# # specify just one legend
-l = plt.legend(handles, labels)
+bp.set(xlabel=None, ylabel='%GFP+ cells Normalized to Day 3')
+handles, labels = bp.get_legend_handles_labels()
 
+# When creating the legend, only use the first four elements to effectively remove the last four.
+l = plt.legend(handles[0:4], labels[0:4])
 
 plt.show()
-
-# sns.boxplot(x = "Day", y = "%GFP cells", data = I2, width=0.5, palette="colorblind")
-# sns.stripplot(x='Day', y= '%GFP cells' , data=I2, jitter=True, marker='o', alpha=0.75, color='black')
-# plt.show()
-
-# # make boxplot with Seaborn
-# bplot=sns.boxplot(y='Day 13', x='Sample', 
-#                  data=dataset, 
-#                  width=0.5,
-#                  palette="colorblind")
- 
-# # add stripplot to boxplot with Seaborn
-# bplot=sns.stripplot('Day 13', x='Sample', 
-#                    data=dataset, 
-#                    jitter=True, 
-#                    marker='o', 
-#                    alpha=0.5,
-#                    color='black')
-
-# plt.show
