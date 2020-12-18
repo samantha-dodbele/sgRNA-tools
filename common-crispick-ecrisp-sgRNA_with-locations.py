9ae450b0101c0c896bc32dad7a749f577065b0ed
samantha-dodbele <59745443+samantha-dodbele@users.noreply.github.com>
2020-12-18 17:56:25 -0500

    preserved corresponding genome locations

--- common-crispick-ecrisp-sgRNA_with-locations.py
+++ common-crispick-ecrisp-sgRNA_with-locations.py
@@ -21,48 +21,18 @@ end = df['end']
 
 ecrisp= df['ecrisp'].str.replace(r' NGG$', '')
 
-# Combine both dataframes into two columns and write to csv file
+# Combine all dataframes into four columns and write to csv file
 combined = pd.concat([crispick, ecrisp, start, end], join = 'outer', axis = 1) 
 combined.to_csv(r'./output/compare_guides.txt', sep='\t', index = False)
 
 # Read in txt file that contains crispick and ecrisp guides
 df = pd.read_csv(r'.\output\compare_guides.txt', sep='\t')
 
+# finds the instersection of the crispick and ecrisp sgRNAs and preserves the genome locations associated with the ecrisp list
+intersection = df.merge(df, how = 'inner', left_on = ['crispick'], right_on=['ecrisp'])
+intersection.drop(columns=['crispick_x', 'crispick_y','ecrisp_x','start_x','end_x'], inplace = True)
+intersection.rename(columns={'ecrisp_y': 'Nucleotide sequence', 'start_y': 'Start', 'end_y': 'End'}, inplace = True)
+print(intersection)
 
-list1 = df['crispick'].tolist()
-list2 = df['ecrisp'].tolist()
-
-s_list1 = set(list1)
-s_list2 = set(list2)
-
-# sgRNAs in common between  two lists
-intersection_1 = s_list1.intersection(s_list2)
-intersection_as_list = list(intersection_1)
-
-new = pd.DataFrame(intersection_as_list, columns=["Nucleotide sequence"])
-new['Start'] = start
-new['End'] = end
-new.to_csv(r'.\output\common_sgrnas.txt', sep='\t', index=False)
-
-# # common in between 1 and 2
-# print("Common in between 1 and 2: "+str(len(intersection_1)))
-
-# with open('./output/common_sgrnas.txt', 'w') as f:
-#     f.write('\n'.join(intersection_as_list))
-
-# Read in csv file containing common guides 
-# top = ["Nucleotide sequence", "Start", "End"]
-# df = pd.read_csv(r'.\output\common_sgrnas.txt', sep='\t', header= top)
-
-
-
-# length = df.size
-# fasta = []
-# for x in range(0, length):
-#      fasta.append("> seq"+str(x+1))
-#      fasta.append(str(df.iloc[x, 0]))
-
-# with open('./output/fasta.txt','w') as f:
-#       f.write('\n'.join(fasta))
-    
-
+# write the common sgRNAs and the start and end locations out to a new text file
+intersection.to_csv(r'.\output\common_sgrnas.txt', sep='\t', index=False)
